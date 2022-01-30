32b21101a2304eec62410a3c6eaaf86fcbf34e7a
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-01-29 21:37:08 -0700

    Replace the unnecessary directions array

--- python/ccc21j3.py
+++ python/ccc21j3.py
@@ -4,16 +4,15 @@ while instructions[-1] != "99999":
     instructions.append(input())
 instructions.pop()
 
-directions = []
 for instruction in instructions:
     direction = int(instruction[0]) + int(instruction[1])
     steps = instruction[2:]
 
     if direction % 2 == 1:
         print("left " + steps)
-        directions.append("left ")
+        last_direction = "left "
     elif direction == 0:
-        print(directions[-1] + steps)
+        print(last_direction + steps)
     else:
         print("right " + steps)
-        directions.append("right ")
+        last_direction = "right "
