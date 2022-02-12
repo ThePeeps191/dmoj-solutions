5dbbc6de73e87f8ef28566358b09c252c39463c6
The Peeps191 <thepeeps.wang@gmail.com>
2022-02-12 14:04:04 -0700

    add problems

--- python/ccc09j2 second solution.py
+++ python/ccc09j2 second solution.py
@@ -1,1 +1,14 @@
-a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# solution work in progress
+
+a, b, c, score = 1, 2, 3, 2#int(input()), int(input()), int(input()), int(input())
+t = 0
+
+for brown in range(a):
+	for north in range(b):
+		for yellow in range(c):
+			print(brown, north, yellow)
+			if brown + north + yellow == score:
+				t += 1
+				print(f"{brown} Brown Trout, {north} Northern Pike, {yellow} Yellow Pickerel")
+
+print(f"Number of ways to catch fish: {t}")
