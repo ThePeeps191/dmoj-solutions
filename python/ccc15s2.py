921b9107e938d666fc4622b93515b1f619eebb66
The Peeps191 <thepeeps.wang@gmail.com>
2022-04-05 19:39:39 -0600

    add problems

--- python/ccc15s2.py
+++ python/ccc15s2.py
@@ -1,14 +1,14 @@
-# in progress
-
 J = int(input())
 A = int(input())
-size = [input() for _ in range(J)]
+size_values = {"S": 0, "M": 1, "L" : 2}
+size = [size_values[input()] for _ in range(J)]
 athlete = [input().split() for _ in range(A)]
 count = 0
 for a in athlete:
 	s, num = a
+	s = size_values[s]
 	num = int(num)
-	if size[num - 1] == s:
+	if size[num - 1] >= s:
 		count += 1
-		size[num - 1] = "N"
+		size[num - 1] = -1
 print(count)
